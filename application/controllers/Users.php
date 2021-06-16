6d4b59279be842abc16d31aaecf014a187b4c19a
iida-takuya <82427718+iida-takuya@users.noreply.github.com>
2021-06-16 10:37:26 +0900

    create an user

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -11,13 +11,37 @@ class Users extends CI_Controller {
 
     public function index()
     {
-        // Use method get_users of user model to get users list
         $data["users"] = $this->users_model->get_users();
-
-        $this->load->view('header'); 
-
-        // assign users list to view
+    
+        // declaring page_title variable
+        $data["page_title"] = "List Of Users";
+    
+        // Assign page title data to header
+        $this->load->view('header', $data); 
         $this->load->view('users/index', $data);
         $this->load->view('footer');
     }
+
+    public function create()
+    {
+        $this->load->helper('form');
+        $this->load->library('form_validation');
+        $this->load->helper('url');
+
+        $data["page_title"] = "Create New User";
+        $this->form_validation->set_rules('first_name', 'First name', 'required');
+        $this->form_validation->set_rules('last_name', 'Last name', 'required');
+        $this->form_validation->set_rules('email', 'Email', array('required','valid_email'));
+
+        if ($this->form_validation->run() === FALSE) {
+            $this->load->view('header', $data); 
+            $this->load->view('users/create', $data);
+            $this->load->view('footer');
+        } else {
+            $this->users_model->create_user();
+            redirect(base_url('/'));
+        }
+    }
+
 }
+
