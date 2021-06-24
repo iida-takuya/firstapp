d8f08d029ec2ea017aba19ee643061e41a5f2bb2
iida-takuya <82427718+iida-takuya@users.noreply.github.com>
2021-06-24 09:16:27 +0900

    homework

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -1,23 +1,24 @@
 <?php
-class Users extends CI_Controller {
+class Users extends CI_Controller
+{
 
     public function __construct()
     {
         parent::__construct();
-
-        // This line will load user model to this controller
         $this->load->model('users_model');
+        $this->load->helper('url');
     }
 
     public function index()
-    {
+    {    
+        $this->load->helper('form');
+        $this->load->library('form_validation');
+
         $data["users"] = $this->users_model->get_users();
-    
-        // declaring page_title variable
+
         $data["page_title"] = "List Of Users";
-    
-        // Assign page title data to header
-        $this->load->view('header', $data); 
+        
+        $this->load->view('header', $data);
         $this->load->view('users/index', $data);
         $this->load->view('footer');
     }
@@ -32,9 +33,9 @@ class Users extends CI_Controller {
         $this->form_validation->set_rules('first_name', 'First name', 'required');
         $this->form_validation->set_rules('last_name', 'Last name', 'required');
         $this->form_validation->set_rules('email', 'Email', array('required','valid_email'));
-
+        $this->form_validation->set_rules('phone_number', 'Phone Number', 'required');
         if ($this->form_validation->run() === FALSE) {
-            $this->load->view('header', $data); 
+            $this->load->view('header', $data);
             $this->load->view('users/create', $data);
             $this->load->view('footer');
         } else {
@@ -43,7 +44,6 @@ class Users extends CI_Controller {
         }
     }
 
-
     public function update($user_id)
     {
         $this->load->helper('form');
@@ -51,12 +51,14 @@ class Users extends CI_Controller {
 
         $data["user"] = $this->users_model->get_user($user_id);
 
-        $data["page_title"] = "Update User";
+        $data["page_title"] = "update User";
+        
         $this->form_validation->set_rules('first_name', 'First name', 'required');
-        $this->form_validation->set_rules('last_name', 'Last name', 'required');
-        $this->form_validation->set_rules('email', 'Email', array('required','valid_email'));
+        $this->form_validation->set_rules('last_name', 'First name', 'required');
+        $this->form_validation->set_rules('email', 'Email', array('required', 'valid_email'));
+        $this->form_validation->set_rules('phone_number', 'Phone Number', 'required');
         if ($this->form_validation->run() === FALSE) {
-            $this->load->view('header', $data); 
+            $this->load->view('header', $data);
             $this->load->view('users/update', $data);
             $this->load->view('footer');
         } else {
@@ -65,10 +67,29 @@ class Users extends CI_Controller {
         }
     }
 
+    public function detail($user_id)
+    {
+        $data["user"] = $this->users_model->get_user($user_id);
+
+        $data["page_title"] = "detail User";
+
+        $this->load->view('header', $data); 
+        $this->load->view('users/detail', $data);
+        $this->load->view('footer');
+    }
+
     public function delete($user_id)
     {
         $this->users_model->delete_user($user_id);
         redirect(base_url('/'));
     }
 
+    public function delete_multi()
+    {
+        $user_ids = $this->input->post('ids');
+        if ($user_ids !== null) {
+            $this->users_model->delete_users($user_ids);
+        }
+        redirect(base_url('/'));
+    }
 }
